bcb514f0100c094a8dad46b47cd4ca650bdeec02
seolhee2750 <2750seolhee@naver.com>
2022-08-22 20:27:20 +0900

    [add] BOJ #12865 평범한 배낭 주석 추가

--- BOJ/src/DP/BOJ_12865.java
+++ BOJ/src/DP/BOJ_12865.java
@@ -7,10 +7,24 @@ package DP;
  * 
  * 물건마다 가능한 최대 가치를 구해주는 것을 반복하는 방식으로 구현
  * memory[i-1][j]와 memory[i-1][j-w]+v 중 더 큰 값을 골랐음 (현재 물건을 넣는 것이 유리한지, 안넣는 것이 유리한지 판단)
+ * 
+ * 입력
+ * 4 7
+ * 6 13
+ * 4 8
+ * 3 6
+ * 5 12
+ * 
+ * memory
+ * [0, 0, 0, 0, 0, 0, 0, 0]
+ * [0, 0, 0, 0, 0, 0, 13, 13]
+ * [0, 0, 0, 0, 8, 8, 13, 13]
+ * [0, 0, 0, 6, 8, 8, 13, 14]
  */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_12865 {
@@ -35,6 +49,9 @@ public class BOJ_12865 {
 			}
 		}
 		
+		for(int i = 0; i < n; i++) { 
+			System.out.println(Arrays.toString(memory[i]));
+		}
 		System.out.println(memory[n][k]);
 	}
 
