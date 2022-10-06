ec1f528123369712dabede04b318bd5a2a7a5791
seolhee2750 <2750seolhee@naver.com>
2022-10-06 14:19:04 +0900

    [change] SWEA #3307 최장증가부분수열 코드 수정

--- SWEA/src/D3/SWEA_3307.java
+++ SWEA/src/D3/SWEA_3307.java
@@ -2,14 +2,32 @@ package D3;
 
 /*
  * SWEA #3307 최장증가부분수열
- * DP, LIS 문제
+ * BS로 풀이
  */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class SWEA_3307 {
+	
+	static List<Integer> lis;
+	
+	public static int BS(int start, int end, int value) { 
+		int s = start;
+		int e = end;
+		int m = 0;
+		
+		while(s < e) {
+			m = (s + e) / 2;
+			if(value <= lis.get(m)) e = m;
+			else s = m + 1;
+		}
+		
+		return s;
+	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -20,22 +38,20 @@ public class SWEA_3307 {
 		for(int t = 1; t <= tc; t++) {
 			int n = Integer.parseInt(in.readLine());
 			int[] nums = new int[n];
-			int[] lis = new int[n];
-			int max = 0;
+			lis = new ArrayList<>();
 			
 			st = new StringTokenizer(in.readLine());
 			for(int i = 0; i < n; i++) {
 				nums[i] = Integer.parseInt(st.nextToken());
-				lis[i] = 1;
-				for(int j = 0; j < i; j++) {
-					if(nums[i] > nums[j] && lis[i] < lis[j]+1) {
-						lis[i] = lis[j] + 1;
-					}
-				}
-				max = Math.max(max, lis[i]);
 			}
-			out.append(String.format("#%d %d\n", t, max));
+			
+			for(int i = 0; i < n; i++) {
+				if(lis.isEmpty() || lis.get(lis.size()-1) < nums[i]) lis.add(nums[i]);
+				else lis.set(BS(0, lis.size()-1, nums[i]),  nums[i]);
+			}
+			
+			out.append("#" + t + " " + lis.size() + "\n");
 		}
-		System.out.println(out);
+		System.out.print(out);
 	}
 }
