f1391d31a4263c9f5fdc18d62e32d028389f2604
김설희 <2750seolhee@naver.com>
2023-01-17 21:01:06 +0900

    [create] BOJ #2558 A+B - 2

---
+++ BOJ/src/Implementation/BOJ_2558.java
@@ -0,0 +1,13 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2558 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(Integer.parseInt(in.readLine()) + Integer.parseInt(in.readLine()));
+	}
+}
